575f16062286457e7b7615508b21cefb00d5c78e
Kyng <155396364+Kyng-dev@users.noreply.github.com>
2024-01-12 12:42:13 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/Kyng-dev/Whaticket-vers-o-2 /home/deploy/whaticket/
+  git clone https://github.com/canove/whaticket-community /home/deploy/whaticket/
 EOF
 
   sleep 2
